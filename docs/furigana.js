0e8da581eb5498bc28edeff7f2fa69433f95e660
Howard Ricketts <ricketts30@yahoo.com>
2018-09-23 23:31:47 +0100

    adding attributes for hiding & obscuring

--- docs/furigana.js
+++ docs/furigana.js
@@ -14,17 +14,27 @@
              options);
 
         // in the first version we only highlight ruby sections
-        this.find("ruby").css("color", settings.ruby_color);
-        this.find("rt").css("color", settings.rt_color);
-        this.find("rt").css("background-color", settings.rt_backgroundColor);
+        this.find("rt").css("color", settings.color);
+        this.find("rt").css("background-color", settings.backgroundColor);
         return this;
     };
 
 }( jQuery ));
 
 jQuery.fn.furigana.defaults = {
-    ruby_color: "green",
-    rt_color: "red",
-    rt_backgroundColor: "yellow",
-    show_level: 1
+    color: "inherit", 
+    backgroundColor: "inherit", 
+    obscure_color: "silver", 
+    obscure_backgroundColor: "silver", 
+    hide_level: 0,
+    obscure_level: 1
 };
+
+/* todo: other attributes
+obscure_color: (default to silver)
+obscure_backgroundColor: (default to silver)
+hide_level: (default of zero, the level at which (and below) to hide furigana)
+obscure_level: (default of one, the level at which (and below) to obscure furigana)
+(note other levels above obscure would be shown)
+a <rt> without attributes or classes is considered to be level 1
+*/
