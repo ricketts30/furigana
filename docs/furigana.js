fecc9fc27aaee59d3da45ad5115ff39bd19cafe8
Howard Ricketts <ricketts30@yahoo.com>
2018-09-23 19:38:46 +0100

    proper plugin management & global settings

--- docs/furigana.js
+++ docs/furigana.js
@@ -2,10 +2,29 @@
 // dependecoes: jQuery (v ??? or higher)
 
 // q.v.: https://learn.jquery.com/plugins/basic-plugin-creation/
+// q,v,: https://learn.jquery.com/plugins/advanced-plugin-concepts/
 
-$.fn.furigana = function() {
-    // in the first version we only highlight ruby sections
-    this.find("ruby").css("color","green");
-    this.find("rt").css("color","red");
-    return this;
-}
+(function ( $ ) {
+
+    $.fn.furigana = function( options ) {
+
+        var settings = $.extend(
+            {}, 
+            $.fn.furigana.defaults,
+             options);
+
+        // in the first version we only highlight ruby sections
+        this.find("ruby").css("color", settings.ruby_color);
+        this.find("rt").css("color", settings.rt_color);
+        this.find("rt").css("background-color", settings.rt_backgroundColor);
+        return this;
+    };
+
+}( jQuery ));
+
+jQuery.fn.furigana.defaults = {
+    ruby_color: "green",
+    rt_color: "red",
+    rt_backgroundColor: "yellow",
+    show_level: 1
+};
